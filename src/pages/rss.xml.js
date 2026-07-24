416b0130773d06be66774a81547f1d14b12e321d
limsungmin <limsungmin2000@gmail.com>
2026-07-24 11:47:53 +0900

    링크 오작동 수정 테스트

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,7 @@ export async function GET(context) {
         items: posts.map((item) => ({
             title: item.data.title,
             description: item.data.excerpt,
-            link: `/blog/${item.id}/`,
+            link: `${import.meta.env.BASE_URL}/blog/${item.id}/`, // BASE_URL 반영
             pubDate: item.data.publishDate.setUTCHours(0)
         }))
     });
